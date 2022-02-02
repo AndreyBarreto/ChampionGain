6c632af4a07d6a24d1438ffcd68866a9b78ba9e7
Andrey Vieira Barreto <andrey.barreto@DEV-01.advice_health.corp>
2022-02-02 19:31:07 -0300

    feature/react portal for modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,24 @@
 import { Overlay, Container, Footer } from "./styles"
 import Button from '../Button'
+import reactDom from "react-dom"
+
 export default function Moldal({ danger }) {
-  return (
+  return reactDom.createPortal(
     <Overlay>
-      <Container danger={danger}>
-        <h1>Título do Modal</h1>
-        <p>corpo do modal</p>
-        <Footer>
-          <button className="cancel-button" type="button">
-            Cancelar
-          </button>
-          <Button type="button" danger={danger}>
-            Deletar
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>
+    <Container danger={danger}>
+      <h1>Título do Modal</h1>
+      <p>corpo do modal</p>
+      <Footer>
+        <button className="cancel-button" type="button">
+          Cancelar
+        </button>
+        <Button type="button" danger={danger}>
+          Deletar
+        </Button>
+      </Footer>
+    </Container>
+  </Overlay>,
+  document.getElementById('modal-root')
   )
+
 }
