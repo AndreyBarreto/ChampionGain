03a5e986fa549b741c5001e29769dff0dc3680a7
Andrey Vieira Barreto <andrey.barreto@DEV-01.advice_health.corp>
2022-03-09 09:01:35 -0300

    feature/useError custom hook

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -5,6 +5,7 @@ import Select from '../Select'
 import Button from '../Button'
 import { useState } from 'react'
 import { isEmailValid } from '../../utils'
+import useErrors from '../../hooks/userErrors'
 
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('')
@@ -12,6 +13,8 @@ export default function ContactForm({ buttonLabel }) {
   const [phone, setPhone] = useState('')
   const [category, setCategory] = useState('')
   const [errors, setErrors] = useState([])
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors()
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -22,10 +25,10 @@ export default function ContactForm({ buttonLabel }) {
     setName(event.target.value)
 
     if (!event.target.value) {
-      setErrors((prevState) => [...prevState, { field: "name", message: "Nome é obrigatório" }])
+      setError({ field: "name", message: "Nome é obrigatório" })
     }
     else {
-      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'))
+      removeError('name')
     }
   }
 
@@ -33,19 +36,11 @@ export default function ContactForm({ buttonLabel }) {
     setEmail(event.target.value)
 
     if (event.target.value && !isEmailValid(event.target.value)) {
-      const errorAlreadyExist = errors.find((error) => error.field === 'email')
-      if (errorAlreadyExist) {
-        return
-      }
-      setErrors((prevState) => [...prevState, { field: "email", message: "E-mail inválido" }])
+      setError({ field: "email", message: "E-mail inválido" })
     }
     else {
-      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'))
+      removeError('email')
     }
-  }
-
-  function getErrorMessageByFieldName(fieldname) {
-    return errors.find((error) => error.field === fieldname)?.message
   }
 
   return (
